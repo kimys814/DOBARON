46d8bbb1b4bbe5e9bb0d2e34ce85e606f41dbd11
javakings <javakings@naver.com>
2013-05-16 03:41:08 +0900

    coupon DTO

---
+++ Webproject/DAO&DTO/clientcouponDTO.java
@@ -0,0 +1,12 @@
+package project;
+
+public class clientcouponDTO {
+	private String somae;
+	
+	public String getSomae() {
+		return somae;
+	}
+	public void setSomae(String somae) {
+		this.somae = somae;
+	}
+}
